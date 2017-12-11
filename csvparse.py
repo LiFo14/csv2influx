2b0af7c643ad325f5ddad176409ebcc095aaa42c
Nick <cyberdak095@gmail.com>
2017-12-11 20:59:45 +0300

    Upload to db by 1 row

--- csvparse.py
+++ csvparse.py
@@ -1,28 +1,36 @@
 #!/usr/bin/python3
 
-from influxdb import InfluxDBClient
 import sys
 import csv
 import json
-import pprint
+from datetime import datetime
+from pprint import pprint
+from influxdb import InfluxDBClient
 
 HOSTNAME = "localhost" #ПЕРВАЯ ЧАСТЬ ДО ТОЧКИ
-MEASUREMENT = "CPUPERF" #ВТОРАЯ ЧАСТЬ ДО .CSV
 
 INFLUX_SERVER_IP = "localhost"
 INFLUX_SERVER_DATABASE = "test"
 INFLUX_SERVER_PORT = "8086"
-INFLUX_MEASUREMENT_ID = MEASUREMENT
+INFLUX_MEASUREMENT_ID = "CPUPERF" #ВТОРАЯ ЧАСТЬ ДО .CSV
 
 def make_influx_dict(table_data):
+    d = datetime.strptime(table_data['EndDateTime'],'%m/%d/%y %H:%M:%S')
     data = [{
-        "measurement": MEASUREMENT,
+        "measurement": INFLUX_MEASUREMENT_ID,
         "tags": {
            "host": HOSTNAME
-         },
+        },
+        "time": d.strftime('%Y-%m-%dT%H:%m:%SZ'),
         "fields": table_data,
     },]
     return data
+
+def upload_data(data):
+    client = InfluxDBClient(INFLUX_SERVER_IP, INFLUX_SERVER_PORT, '', '', INFLUX_SERVER_DATABASE)
+    client.write_points(data,database='test')
+    #client.write(data)
+    #pprint(client.query('SELECT * FROM CPUPERF', database='test'))
 
 def read_csv(file):
     with open(file) as csvfile:
@@ -30,14 +38,12 @@ def read_csv(file):
         reader = csv.reader(csvfile)
         values = []
         point = {}
-        for value in zip(*reader):
-            values.append(','.join(value))
-        for title, value in zip(titles, values):
-            point[title] = value
-        return make_influx_dict(point)
+        for row in reader:
+            if len(row):
+                for title, value in zip(titles, row):
+                    point[title] = value
+                data = make_influx_dict(point)
+                upload_data(data)
 
 if __name__ == "__main__":
-    data = read_csv(sys.argv[1])
-    pprint.pprint(data)
-    influxclient = InfluxDBClient(INFLUX_SERVER_IP, INFLUX_SERVER_PORT, '', '', INFLUX_SERVER_DATABASE)
-    influxclient.write_points(data,database='test') 
+    read_csv(sys.argv[1])
